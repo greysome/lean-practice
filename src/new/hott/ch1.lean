9ed59a0f5cea39ad556f30b338e4304f104ad036
greysome <wayyan.win@gmail.com>
2022-10-27 12:17:06 +0800

    I learnt that you have to manually tell lean to use the eliminator elaboration strategy and that fixes the problem of it being too dumb

--- src/new/hott/ch1.lean
+++ src/new/hott/ch1.lean
@@ -14,48 +14,51 @@ notation `rfl` := Eq.refl _
 
 namespace Eq
 -- Path induction
-def ind (C : Π (a b : α), a = b → Type*)
+@[elab_as_eliminator]
+def ind {C : Π (a b : α), a = b → Type*}
   (c : Π (a : α), C a a rfl) :
   Π (a b : α) (p : a = b), C a b p :=
 λ a b p, @Eq.rec α a (C a) (c a) b p
 
 -- Based path induction
+@[elab_as_eliminator]
 def ind' (a : α)
-  (C : Π (b : α), a = b → Type*)
+  {C : Π (b : α), a = b → Type*}
   (c : C a rfl) :
   Π (b : α) (p : a = b), C b p :=
 λ b p, @Eq.rec α a C c b p
 
 -- Based path induction, proven using `ind`
 def ind'₁ (a : α)
-  (C : Π (b : α), a = b → Type*)
+  {C : Π (b : α), a = b → Type*}
   (c : C a rfl) :
   Π (b : α) (p : a = b), C b p :=
 let D : Π (a b : α), a = b → Type* :=
     λ a b p, Π (C : Π (z : α), a = z → Type*), C a rfl → C b p,
   d : Π (a : α), D a a rfl := λ x C, id,
-  f : Π (a b : α) (p : a = b), D a b p := Eq.ind D d in
+  f : Π (a b : α) (p : a = b), D a b p := Eq.ind d in
 λ b p, f a b p C c
 
--- Exercise 1.15: indiscernability of identicals (in a sense the recursor)
--- follows from path induction
-def rec' (P : α → Type*) :
-  Π {a b : α} (p : a = b), P a → P b :=
-let C := λ (a b : α) (p : a = b), P a → P b in
-Eq.ind C (λ a c, c)
+-- Exercise 1.15: indiscernability of identicals follows from path induction
+-- The justification for the name `transport` is in ch2
+@[elab_as_eliminator]
+def transport {P : α → Type*} {a b : α} (p : a = b) : P a → P b :=
+Eq.ind (λ a, @id (P a)) a b p
 
-example {P : α → Type*} {a : α} : rec' P (Eq.refl a) = id := rfl
+@[elab_as_eliminator]
+def ap (f : α → β) {a b : α} (p : a = b) : f a = f b :=
+transport p (refl (f a))
+
+example {P : α → Type*} {a : α} : @@transport P (refl a) = id := rfl
+example {f : α → β} {a : α} : ap f (refl a) = refl (f a) := rfl
 
 -- The following results were not proven in ch1 but they are needed
 -- for some of the exercises
 def symm {a b : α} : a = b → b = a :=
-λ p, @Eq.rec' α (λ x, x = a) a b p rfl
+λ p, transport p (refl a)
 
 def trans {a b c : α} : a = b → b = c → a = c :=
-λ p, Eq.rec' (λ x, x = c → a = c) p id
-
-def ap (f : α → β) {a b : α} (p : a = b) : f a = f b :=
-@Eq.rec' α (λ x, f a = f x) _ _ p rfl
+λ p, transport p (@id (a = c))
 
 end Eq
 
@@ -74,7 +77,8 @@ def rec' : (α → β → γ) → α × β → γ := @prod.rec α β (λ _, γ)
 
 -- The induction principle, i.e. dependent eliminator
 -- This is just the Lean-given `prod.rec` with rearranged arguments
-def ind (C : α × β → Type*)
+@[elab_as_eliminator]
+def ind {C : α × β → Type*}
   (c : Π (a : α) (b : β), C ⟨a, b⟩) :
   Π (x : α × β), C x :=
 @prod.rec α β C c
@@ -90,8 +94,7 @@ example {a : α} {b : β} : pr₂ ⟨a, b⟩ = b := rfl
 
 -- The uniqueness principle
 def uniq : Π (x : α × β), x = ⟨x.pr₁, x.pr₂⟩ :=
-let C : α × β → Type* := λ x, x = ⟨x.pr₁, x.pr₂⟩ in
-prod.ind C (λ a b, rfl)
+@@prod.ind (λ x, x = ⟨x.pr₁, x.pr₂⟩) (λ a b, rfl)
 
 end prod
 
@@ -109,7 +112,8 @@ variable {f : α → Type*}
 def rec' : (Π (a : α), f a → β) → (Σ (a : α), f a) → β :=
 @sigma.rec α f (λ _, β)
 
-def ind (C : (Σ (a : α), f a) → Type*)
+@[elab_as_eliminator]
+def ind {C : (Σ (a : α), f a) → Type*}
   (c : Π (a : α) (b : f a), C ⟨a, b⟩) :
   Π (p : Σ (a : α), f a), C p :=
 @sigma.rec α f C c
@@ -117,8 +121,7 @@ def ind (C : (Σ (a : α), f a) → Type*)
 def pr₁ : (Σ (a : α), f a) → α := sigma.rec' (λ a b, a)
 
 def pr₂ : Π (x : Σ (a : α), f a), f x.pr₁ :=
-let C := λ (x : Σ (a : α), f a), f x.pr₁ in
-sigma.ind C (λ a b, b)
+@@sigma.ind f (λ (x : sigma f), f x.pr₁) (λ a b, b)
 
 -- A naive type-theoretic formulation of the axiom of choice,
 -- which is trivial in this case.
@@ -139,7 +142,8 @@ infixr ` ⊕ `:34 := sum
 namespace sum
 def rec' : (α → γ) → (β → γ) → α ⊕ β → γ := @sum.rec α β (λ _, γ)
 
-def ind (C : α ⊕ β → Type*) :
+@[elab_as_eliminator]
+def ind {C : α ⊕ β → Type*} :
   (Π (a : α), C (inl a)) → (Π (b : β), C (inr b)) → Π (x : α ⊕ β), C x :=
 λ c, @sum.rec α β C c
 
@@ -152,7 +156,8 @@ inductive empty : Type
 namespace empty
 def rec' : empty → α := empty.rec (λ _, α)
 
-def ind (C : empty → Type*) : Π (x : empty), C x := empty.rec C
+@[elab_as_eliminator]
+def ind {C : empty → Type*} : Π (x : empty), C x := empty.rec C
 
 end empty
 
@@ -164,11 +169,11 @@ inductive unit
 namespace unit
 def rec' : α → unit → α := @unit.rec (λ _, α)
 
-def ind : Π (C : unit → Type*), C star → Π (x : unit), C x := @unit.rec
+@[elab_as_eliminator]
+def ind : Π {C : unit → Type*}, C star → Π (x : unit), C x := @unit.rec
 
 def uniq : Π (x : unit), x = star :=
-let C : unit → Type* := λ x, x = star in
-unit.ind C rfl
+@@unit.ind (λ x, x = star) rfl
 
 end unit
 
@@ -181,11 +186,11 @@ inductive bool
 namespace bool
 def rec' : α → α → bool → α := @bool.rec (λ _, α)
 
-def ind : Π (C : bool → Type*), C ff → C tt → Π (x : bool), C x := @bool.rec
+@[elab_as_eliminator]
+def ind : Π {C : bool → Type*}, C ff → C tt → Π (x : bool), C x := @bool.rec
 
 def uniq : Π (x : bool), (x = ff) ⊕ (x = tt) :=
-let C : bool → Type* := λ x, (x = ff) ⊕ (x = tt) in
-bool.ind C (sum.inl rfl) (sum.inr rfl)
+@@bool.ind (λ x, (x = ff) ⊕ (x = tt)) (sum.inl rfl) (sum.inr rfl)
 
 end bool
 
@@ -200,7 +205,8 @@ notation `ℕ` := nat
 namespace nat
 def rec' : α → (ℕ → α → α) → ℕ → α := @nat.rec (λ _, α)
 
-def ind : Π (C : ℕ → Type*), C zero →
+@[elab_as_eliminator]
+def ind : Π {C : ℕ → Type*}, C zero →
   (Π (n : ℕ), C n → C (succ n)) → Π (n : ℕ), C n :=
 @nat.rec
 
@@ -257,35 +263,35 @@ def sigma.rec₁ : (Π (a : α), f a → β) → (Σ (a : α), f a) → β := λ
 example {g : Π (a : α), f a → β} {a : α} {b : f a} : sigma.rec₁ g ⟨a, b⟩ = g a b := rfl
 
 -- Exercise 1.3
-def prod.ind₁ (C : α × β → Type*)
+def prod.ind₁ {C : α × β → Type*}
   (c : Π (a : α) (b : β), C ⟨a, b⟩) :
   Π (x : α × β), C x :=
-λ x, Eq.rec' C x.uniq.symm (c x.pr₁ x.pr₂)
+λ x, Eq.transport x.uniq.symm (c x.pr₁ x.pr₂)
 
 -- Full derivation:
--- prod.ind₁ C c ⟨a, b⟩
--- ≡ Eq.rec' C ⟨a, b⟩ ⟨a, b⟩ ⟨a, b⟩.uniq.symm (c a b)
--- ≡ Eq.rec' C ⟨a, b⟩ ⟨a, b⟩ rfl (c a b)
+-- prod.ind₁ c ⟨a, b⟩
+-- ≡ Eq.transport ⟨a, b⟩.uniq.symm (c a b)
+-- ≡ Eq.transport rfl (c a b)
 -- ≡ id (c a b)
 -- ≡ (c a b)
 example {C : α × β → Type*} {c : Π (a : α) (b : β), C ⟨a, b⟩}
-  {a : α} {b : β} : prod.ind₁ C c ⟨a, b⟩ = c a b :=
+  {a : α} {b : β} : prod.ind₁ c ⟨a, b⟩ = c a b :=
 rfl
 
 def sigma.uniq : Π (x : Σ (a : α), f a), x = ⟨x.pr₁, x.pr₂⟩ :=
 let C := λ (x : Σ (a : α), f a), x = ⟨x.pr₁, x.pr₂⟩ in
-sigma.ind C (λ a b, rfl)
+@@sigma.ind f C (λ a b, rfl)
 
 -- Basically the same proof as prod.ind₁ (the characters are exactly
 -- the same even!)
-def sigma.ind₁ (C : (Σ (a : α), f a) → Type*)
+def sigma.ind₁ {C : (Σ (a : α), f a) → Type*}
   (c : Π (a : α) (b : f a), C ⟨a, b⟩) :
   Π (x : Σ (a : α), f a), C x :=
-λ x, Eq.rec' C x.uniq.symm (c x.pr₁ x.pr₂)
+λ x, Eq.transport x.uniq.symm (c x.pr₁ x.pr₂)
 
 example {C : (Σ (a : α), f a) → Type*}
   {c : Π (a : α) (b : f a), C ⟨a, b⟩}
-  {a : α} {b : f a} : sigma.ind₁ C c ⟨a, b⟩ = c a b :=
+  {a : α} {b : f a} : sigma.ind₁ c ⟨a, b⟩ = c a b :=
 rfl
 
 -- Exercise 1.4
@@ -324,8 +330,7 @@ example {a : α} {f : ℕ → α → α} : nat.rec₁ a f nat.zero = a := rfl
 -- ≡ ⟨n.succ, f n (nat.rec' a f n)⟩
 -- ≡ ⟨n.succ, nat.rec' a f n.succ⟩
 def aux (a : α) (f : ℕ → α → α) (n : ℕ) : zip a f n = ⟨n, nat.rec' a f n⟩ :=
-let C := λ n, zip a f n = ⟨n, nat.rec' a f n⟩ in
-nat.ind C rfl (λ n h, Eq.ap (G f) h) n
+nat.ind rfl (λ n h, Eq.ap (G f) h) n
 
 -- Full derivation:
 -- nat.rec₁ a f n.succ
@@ -339,7 +344,7 @@ example {a : α} {f : ℕ → α → α} {n : ℕ} :
 let h₁ : (zip a f n.succ).pr₂ = nat.rec' a f n.succ :=
   Eq.ap prod.pr₂ (aux a f n.succ),
 h₂ : f n (nat.rec' a f n) = f n (zip a f n).pr₂ :=
-  @Eq.ap (ℕ × α) α (λ x, f n x.pr₂) _ _ (aux a f n).symm in
+  Eq.ap (λ (x : ℕ × α), f n x.pr₂) (aux a f n).symm in
 Eq.trans h₁ h₂
 
 -- Exercise 1.5
@@ -349,13 +354,13 @@ Eq.trans h₁ h₂
 def sum₁ (α β : Type*) : Type* := Σ (x : bool), bool.rec' α β x
 
 namespace sum₁
-def ind {α β : Type*} (C : sum₁ α β → Type*)
+def ind {α β : Type*} {C : sum₁ α β → Type*}
   (c₁ : Π (a : α), C ⟨bool.ff, a⟩)
   (c₂ : Π (b : β), C ⟨bool.tt, b⟩) :
   Π (x : sum₁ α β), C x :=
-sigma.ind C (
-  let C' : bool → Type* := λ a, Π (b : bool.rec' α β a), C ⟨a, b⟩ in
-  bool.ind C' c₁ c₂
+sigma.ind (
+  let C' := λ a, Π (b : bool.rec' α β a), C ⟨a, b⟩ in
+  @@bool.ind C' c₁ c₂
 )
 
 -- Full derivation:
@@ -367,15 +372,17 @@ example {α β : Type*} {C : sum₁ α β → Type*}
   {c₁ : Π (a : α), C ⟨bool.ff, a⟩}
   {c₂ : Π (b : β), C ⟨bool.tt, b⟩}
   {a : α} :
-sum₁.ind C c₁ c₂ ⟨bool.ff, a⟩ = c₁ a := rfl
+sum₁.ind c₁ c₂ ⟨bool.ff, a⟩ = c₁ a := rfl
 
 example {α β : Type*} {C : sum₁ α β → Type*}
   {c₁ : Π (a : α), C ⟨bool.ff, a⟩}
   {c₂ : Π (b : β), C ⟨bool.tt, b⟩}
   {b : β} :
-sum₁.ind C c₁ c₂ ⟨bool.tt, b⟩ = c₂ b := rfl
+sum₁.ind c₁ c₂ ⟨bool.tt, b⟩ = c₂ b := rfl
 
 end sum₁
+
+
 
 -- Exercise 1.8, 1.16
 namespace nat
@@ -397,29 +404,26 @@ variables (k n m : ℕ)
 -- I have no idea why the hell removing the @ breaks the proof
 -- Full derivation of the inductive step left as an exercise to the reader
 def add_assoc : (k + n) + m = k + (n + m) :=
-let C := λ k, (k + n) + m = k + (n + m) in
-@nat.ind C rfl (λ k h, Eq.ap succ h) k
+nat.ind rfl (λ k h, Eq.ap succ h) k
 
 def add_zero : n + zero = n :=
-let C := λ n, n + zero = n in
-@nat.ind C rfl (λ k h, Eq.ap succ h) n
+nat.ind rfl (λ k h, Eq.ap succ h) n
 
 -- Recall `zero_add : zero + n = n` is a definitional equality.
 
 -- The proof is not trivial; it requires double induction
 def add_comm : n + m = m + n :=
-let C := λ n, n + m = m + n,
-h : Π (n m : ℕ), n + m = m + n :=
+let h : Π (n m : ℕ), n + m = m + n :=
   (let C₁ := λ n, Π (m : ℕ), n + m = m + n in
-    nat.ind C₁ (λ m, (add_zero m).symm)
+    @@nat.ind C₁ (λ m, (add_zero m).symm)
       (λ n h₁,
         let C₂ := λ m, n.succ + m = m + n.succ in
-          nat.ind C₂ (add_zero n.succ)
+          @@nat.ind C₂ (add_zero n.succ)
             (λ m h₂,
               let h₃ : (n + m.succ).succ = (m.succ + n).succ :=
                 Eq.ap succ (h₁ m.succ),
               h₄ : (m + n).succ.succ = (n + m).succ.succ :=
-                @Eq.ap _ _ (succ ∘ succ) _ _ (h₁ m).symm,
+                Eq.ap (succ ∘ succ) (h₁ m).symm,
               h₅ : (n.succ + m).succ = (m + n.succ).succ :=
                 Eq.ap succ h₂ in
               Eq.trans (Eq.trans h₃ h₄) h₅))) in
@@ -428,18 +432,15 @@ h n m
 def one_mul : zero.succ * k = k := add_zero k
 
 def mul_one : k * zero.succ = k :=
-let C := λ k, k * zero.succ = k in
-@nat.ind C rfl (λ k h, Eq.ap (add zero.succ) h) k
+nat.ind rfl (λ k h, Eq.ap (add zero.succ) h) k
 
 def mul_zero : k * zero = zero :=
-let C := λ k, k * zero = zero in
-@nat.ind C rfl (λ k h, h) k
+nat.ind rfl (λ k h, h) k
 
 -- `zero_mul : zero * k = zero` holds definitionally
 
 def mul_add : k * (n + m) = k * n + k * m :=
-let C := λ k, k * (n + m) = k * n + k * m in
-@nat.ind C rfl (λ k h,
+nat.ind rfl (λ k h,
   let h₁ : (n + m) + k * (n + m) = (n + m) + (k * n + k * m) :=
     Eq.ap (add (n + m)) h,
   h₂ : (n + m) + (k * n + k * m) = n + (m + (k * n + k * m)) :=
@@ -447,7 +448,7 @@ let C := λ k, k * (n + m) = k * n + k * m in
   h₃ : n + (m + (k * n + k * m)) = n + ((m + k * n) + k * m) :=
     Eq.ap (add n) (add_assoc m (k * n) (k * m)).symm,
   h₄ : n + ((m + k * n) + k * m) = n + ((k * n + m) + k * m) :=
-    @Eq.ap _ _ (λ x, n + (x + k * m)) _ _ (add_comm m (k * n)),
+    Eq.ap (λ x, n + (x + k * m)) (add_comm m (k * n)),
   h₅ : n + ((k * n + m) + k * m) = n + (k * n + (m + k * m)) :=
     Eq.ap (add n) (add_assoc (k * n) m (k * m)),
   h₆ : n + (k * n + (m + k * m)) = (n + k * n) + (m + k * m) :=
@@ -455,24 +456,21 @@ let C := λ k, k * (n + m) = k * n + k * m in
   Eq.trans (Eq.trans (Eq.trans (Eq.trans (Eq.trans h₁ h₂) h₃) h₄) h₅) h₆) k
 
 def add_mul : (n + m) * k = n * k + m * k :=
-let C := λ n, (n + m) * k = n * k + m * k in
-@nat.ind C rfl (λ n h,
+nat.ind rfl (λ n h,
   let h₁ : k + (n + m) * k = k + (n * k + m * k) :=
     Eq.ap (add k) h,
   h₂ : k + (n * k + m * k) = (k + n * k) + m * k :=
     (add_assoc _ _ _).symm in Eq.trans h₁ h₂) n
 
 def mul_assoc : (k * n) * m = k * (n * m) :=
-let C := λ k, (k * n) * m = k * (n * m) in
-@nat.ind C rfl (λ k h,
+nat.ind rfl (λ k h,
   let h₁ : (n + k * n) * m = n * m + (k * n) * m :=
     add_mul _ _ _,
   h₂ : n * m + (k * n) * m = n * m + k * (n * m) :=
     Eq.ap (add (n * m)) h in Eq.trans h₁ h₂) k
 
 def mul_comm : n * m = m * n :=
-let C := λ n, n * m = m * n in
-@nat.ind C (mul_zero m).symm (λ n h,
+nat.ind (mul_zero m).symm (λ n h,
   let h₁ : m + n * m = n * m + m :=
     add_comm _ _,
   h₂ : n * m + m = m * n + m :=
--- src/new/hott/ch2.lean
+++ src/new/hott/ch2.lean
@@ -2,7 +2,7 @@ prelude
 import .ch1
 
 variables {α β γ δ : Type*}
-variables {a b c d : α} {p : a = b} {q : b = c} {r : c = d}
+variables {a b c d : α} (p : a = b) (q : b = c) (r : c = d)
 
 namespace Eq
 -- The computation rules associated with various proofs of
@@ -15,11 +15,10 @@ example : rfl ⬝ p = p := rfl
 
 -- Two other proofs of transitivity with different computation rules
 def trans₁ : a = b → b = c → a = c :=
-λ p q, Eq.rec' (λ x, a = x) q p
+λ p q, transport q p
 
 def trans₂ : a = b → b = c → a = c :=
-λ p, Eq.rec' (λ x, x = c → a = c) p
-  (λ q, Eq.rec' (λ x, a = x) q rfl)
+λ p, transport p (λ q, transport q rfl)
 
 example : trans₁ p rfl = p := rfl
 example : trans₂ (refl a) rfl = rfl := rfl
@@ -30,34 +29,50 @@ example : trans₂ (refl a) rfl = rfl := rfl
 -- example : trans₂ rfl p = p := rfl
 
 def concat_rfl : p ⬝ rfl = p :=
-let C : Π (a b : α), a = b → Type* := λ a b p, p ⬝ rfl = p in
-Eq.ind C (λ a, (rfl : refl a ⬝ rfl = rfl)) a b p
+Eq.ind (λ a, (rfl : refl a ⬝ rfl = rfl)) a b p
 
 -- See the example above
 def rfl_concat : rfl ⬝ p = p := rfl
 
 def inv_concat : p⁻¹ ⬝ p = rfl :=
 let C : Π (a b : α), a = b → Type* := λ a b p, p⁻¹ ⬝ p = rfl in
-Eq.ind C (λ a, (rfl : (refl a)⁻¹ ⬝ rfl = rfl)) a b p
+Eq.ind (λ a, (rfl : (refl a)⁻¹ ⬝ rfl = rfl)) a b p
 
 def concat_inv : p ⬝ p⁻¹ = rfl :=
-let C : Π (a b : α), a = b → Type* := λ a b p, p ⬝ p⁻¹ = rfl in
-Eq.ind C (λ a, (rfl : (refl a) ⬝ rfl⁻¹ = rfl)) a b p
+Eq.ind (λ a, (rfl : (refl a) ⬝ rfl⁻¹ = rfl)) a b p
 
 def inv_inv : p⁻¹⁻¹ = p :=
 let C : Π (a b : α), a = b → Type* := λ a b p, p⁻¹⁻¹ = p in
-Eq.ind C (λ a, (rfl : (refl a)⁻¹⁻¹ = rfl)) a b p
+Eq.ind (λ a, (rfl : (refl a)⁻¹⁻¹ = rfl)) a b p
 
 -- As with `Eq.trans`, one can perform induction three ways.
 -- As usual I do induction on `p`, and in fact it's the most
 -- convenient since the proof of the base case follows definitionally.
 def concat_assoc : (p ⬝ q) ⬝ r = p ⬝ (q ⬝ r) :=
-let C : Π (a b : α), a = b → Type* := λ a b p,
-  Π (c d : α) (q : b = c) (r : c = d), (p ⬝ q) ⬝ r = p ⬝ (q ⬝ r) in
-Eq.ind C (λ a c d q r, (rfl : (rfl ⬝ q) ⬝ r = rfl ⬝ (q ⬝ r)))
+Eq.ind (λ a c d q r, (rfl : (rfl ⬝ q) ⬝ r = rfl ⬝ (q ⬝ r)))
   a b p c d q r
 
+variables (f : α → β) (g : β → γ)
 
+def ap_concat : ap f (p ⬝ q) = (ap f p) ⬝ (ap f q) :=
+Eq.ind (λ a c q, rfl) a b p c q
+
+def ap_inv : ap f (p⁻¹) = (ap f p)⁻¹ :=
+Eq.ind (λ a, rfl) a b p
+
+def ap_ap : ap g (ap f p) = ap (g ∘ f) p :=
+Eq.ind (λ a, rfl) a b p
+
+def ap_id : ap id p = p :=
+Eq.ind (λ a, rfl) a b p
+
+end Eq
+
+
+
+open Eq
+
+namespace homotopy
 def pointed : Type* := Σ (α : Type*), α
 
 def loop_space' : ℕ → pointed → pointed :=
@@ -70,8 +85,68 @@ prefix `Ω`:50 := loop_space
 -- How to generalise this notation to all natural arguments?
 prefix `Ω²`:50 := loop_space nat.zero.succ.succ
 
+-- Sanity check that the definition of `loop_space` is correct
 example : Ω² a = (refl a = rfl) := rfl
 
-def eckmann_hilton {A B : Ω² a} : A ⬝ B = B ⬝ A := sorry
+-- The entire following section is devoted to proving Eckmann-Hilton
+def whisker_right {p q : a = b} (A : p = q) (r : b = c) :
+  p ⬝ r = q ⬝ r :=
+Eq.ind (λ b a p q A, p.concat_rfl ⬝ A ⬝ q.concat_rfl⁻¹) b c r a p q A
 
-end Eq
+def whisker_left {r s : b = c} (q : a = b) (B : r = s) :
+  q ⬝ r = q ⬝ s :=
+Eq.ind (λ a c r s B, r.rfl_concat ⬝ B ⬝ s.rfl_concat⁻¹) a b q c r s B
+
+infix ` ⬝r `:50 := whisker_right
+infix ` ⬝l `:50 := whisker_left
+
+-- One could also define `whisker_right` and `whisker_left` as follows
+-- (namely induction on `A`), but we lose our nice judgmental equality:
+example {p q : a = b} (A : p = q) (r : b = c) :
+  p ⬝ r = q ⬝ r :=
+Eq.transport A rfl
+
+example {r s : b = c} (q : a = b) (B : r = s) :
+  q ⬝ r = q ⬝ s :=
+Eq.transport B rfl
+
+def star {p q : a = b} {r s : b = c} (A : p = q) (B : r = s) :
+  p ⬝ r = q ⬝ s :=
+(A ⬝r r) ⬝ (q ⬝l B)
+
+def star₁ {p q : a = b} {r s : b = c} (A : p = q) (B : r = s) :
+  p ⬝ r = q ⬝ s :=
+(p ⬝l B) ⬝ (A ⬝r s)
+
+variables (A B : Ω² a)
+
+-- This should really be done via tactics... 
+def star_eq_concat : star A B = A ⬝ B :=
+let A' := (concat_rfl rfl) ⬝ A ⬝ (concat_rfl rfl)⁻¹,
+  B' := (concat_rfl rfl) ⬝ B ⬝ (concat_rfl rfl)⁻¹,
+  h₁ : A' = A := concat_rfl A,
+  h₂ : B' = B := concat_rfl B,
+  h₃ : A' ⬝ B' = A' ⬝ B := ap (λ x, A' ⬝ x) h₂,
+  h₄ : A' ⬝ B = A ⬝ B := ap (λ x, x ⬝ B) h₁ in
+h₃ ⬝ h₄
+
+def star₁_eq_concat : star₁ A B = B ⬝ A :=
+let A' := (concat_rfl rfl) ⬝ A ⬝ (concat_rfl rfl)⁻¹,
+  B' := (concat_rfl rfl) ⬝ B ⬝ (concat_rfl rfl)⁻¹,
+  h₁ : A' = A := concat_rfl A,
+  h₂ : B' = B := concat_rfl B,
+  h₃ : B' ⬝ A' = B ⬝ A' := ap (λ x, x ⬝ A') h₂,
+  h₄ : B ⬝ A' = B ⬝ A := ap (λ x, B ⬝ x) h₁ in
+h₃ ⬝ h₄
+
+def star_eq_star₁ : star A B = star₁ A B :=
+let C := λ (p q : a = a) (A : p = q), star A B = star₁ A B in
+@@Eq.ind C (λ (p : a = a),
+  let C' := λ (r s : a = a) (B : r = s), star (refl p) B = star₁ (refl p) B in
+  @@Eq.ind C' (λ (r : a = a), _) rfl rfl B
+) rfl rfl A
+
+def eckmann_hilton : A ⬝ B = B ⬝ A :=
+(star_eq_concat A B).symm ⬝ (star_eq_star₁ A B) ⬝ (star₁_eq_concat A B)
+
+end homotopy
